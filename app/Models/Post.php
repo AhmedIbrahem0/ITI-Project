8fb6bf51b5973c4508fa02bbe24b1fc19544f753
ahmed <ahmed@gmail.com>
2021-09-17 12:09:06 +0200

    Second Commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
 
 class Post extends Model
 {
@@ -18,6 +17,9 @@ class Post extends Model
        'excerpt',
        'body'
    ];
+   function comments(){
+      return $this->hasmany(Comment::class);
+   }
    function author(){
        return $this->belongsTo(User::class,"user_id");
    }
